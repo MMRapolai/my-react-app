9ba54600b38e112b39747bbdf53edcc4bfe1676e
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-06 14:51:45 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Search from "./Search";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
