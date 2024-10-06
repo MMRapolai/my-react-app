fdae4cd0c9bee75c90a799fc215a8fce23e4aafd
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-06 20:49:38 +0200

    added a footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <br/>
+      <footer>This project was coded by Matshelane Rapolai and is <a href="https://github.com/MMRapolai/my-react-app/" target="_blank">open-sourced on GitHub</a> and hosted on <a href="https://murielweek4react.netlify.app/" target="_blank">Netlify</a></footer>
     </div>
   );
 }
